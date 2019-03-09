c413823da45135692b4035927dc3a6d5427bb68b
Jadgptw <Gordin.Alexander90@gmail.com>
2019-03-09 14:57:03 +0300

    feat: create addTable function

---
+++ src/addTable.js
@@ -0,0 +1,20 @@
+const addTable = () => {
+  const table = document.createElement('table');
+  table.innerHTML = `
+    <thead>
+      <tr>
+      <th>Transaction ID</th>
+      <th>User Info</th>
+      <th>Order Date</th>
+      <th>Order Amount</th>
+      <th>Card Number</th>
+      <th>Card Type</th>
+      <th>Location</th>
+      </tr>
+    </thead>
+    <tbody></tbody>
+  `;
+  return table;
+};
+
+export default addTable;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import createOrdersList from './createOrdersList';
+import addTable from './addTable';
 
 export default (async function () {
+  const app = document.getElementById('app');
+  app.appendChild(addTable());
+
+  const head = document.querySelector('thead');
+  const body = document.querySelector('tbody');
+
   const ordersList = await createOrdersList();
 
   console.log(ordersList);
