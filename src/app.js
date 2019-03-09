c3bc2e896e985bd0bd6b7ab0b1321d63779e7052
Jadgptw <Gordin.Alexander90@gmail.com>
2019-03-09 15:01:19 +0300

    feat: add sort functions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import createOrdersList from './createOrdersList';
 import addTable from './addTable';
 import addOrders from './addOrders';
+import sortByColumn from './sortByColumn';
 
 export default (async function () {
   const app = document.getElementById('app');
@@ -27,7 +28,30 @@ export default (async function () {
     }
   };
 
+  const handleSort = (event) => {
+    let currentNode = event.target;
+    while(currentNode !== head) {
+      const column = currentNode.textContent.toLowerCase();
+      if(currentNode.nodeName.toLowerCase() === 'th' && column !== 'card number') {
+        const span = document.createElement('span');
+        span.innerHTML = '&#8595;';
+        if(currentNode.children.length !== 1) {
+          currentNode.appendChild(span);
+        }
+        sortByColumn(ordersList, column);
+        body.innerHTML = '';
+        addOrders(body, ordersList);
+        addStatistics(body, ordersList);
+        break;
+      }
+      else {
+        currentNode = currentNode.parentElement;
+      }
+    }
+  };
+
   addOrders(body, ordersList);
 
   body.addEventListener('click', handleShowUserDataInfo);
+  head.addEventListener('click', handleSort);
 }());
---
+++ src/sortByColumn.js
@@ -0,0 +1,72 @@
+const sortOrder = (orderList, element) => {
+  orderList.sort((a, b) => {
+    if (a[element] > b[element]) {
+      return 1;
+    }
+    if (a[element] < b[element]) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+const sortUser = (orderList) => {
+  orderList.sort((a, b) => {
+    if (a.user.lastName > b.user.lastName) {
+      return 1;
+    }
+    if (a.user.lastName < b.user.lastName) {
+      return -1;
+    }
+    if (a.user.firstName > b.user.firstName) {
+      return 1;
+    }
+    if (a.user.firstName < b.user.firstName) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+const sortLocation = (orderList) => {
+  orderList.sort((a, b) => {
+    if (a.orderCountry > b.orderCountry) {
+      return 1;
+    }
+    if (a.orderCountry < b.orderCountry) {
+      return -1;
+    }
+    if (a.orderIp > b.orderIp) {
+      return 1;
+    }
+    if (a.orderIp < b.orderIp) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+const sortByColumn = (orderList, column) => {
+  switch (column) {
+    case 'transaction id':
+      sortOrder(orderList, 'transactionId');
+      break;
+    case 'user info':
+      sortUser(orderList);
+      break;
+    case 'order date':
+      sortOrder(orderList, 'date');
+      break;
+    case 'order amount':
+      sortOrder(orderList, 'amount');
+      break;
+    case 'card type':
+      sortOrder(orderList, 'cardType');
+      break;
+    default:
+      sortLocation(orderList);
+      break;
+  }
+};
+
+export default sortByColumn;
